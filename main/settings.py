face0619ae2c3931cf4b9edbc24bb6c858fa3168
Andres David Mendoza Marquez <andres.david.mm@hotmail.com>
2021-12-26 12:09:49 -0500

    change Procfile name

--- main/settings.py
+++ main/settings.py
@@ -160,11 +160,6 @@ DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (  
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
