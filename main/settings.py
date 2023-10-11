655ff36bc5e319e6f79c32d30a0551f571c0adb1
andreidem18 <75345854+andreidem18@users.noreply.github.com>
2023-10-11 12:03:06 -0500

    Added rate limit

--- main/settings.py
+++ main/settings.py
@@ -127,6 +127,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '30/minute',
+        'user': '30/minute'
+    }
+}
+
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': [
